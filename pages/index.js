1e56757daada72262d9db02a803bcce6f9a3f996
ryanmbigelow <ryanmbigelow@gmail.com>
2023-07-29 00:41:00 -0500

    updated index

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { Button } from 'react-bootstrap';
 import { signOut } from '../utils/auth';
-import { useAuth } from '../utils/context/authContext';
 import { getProducts } from '../utils/data/productData';
 import ProductCard from '../components/product/ProductCard';
 
 function Home() {
-  const { user } = useAuth();
   const [products, setProducts] = useState([]);
   const getAllProducts = () => {
     getProducts().then((data) => setProducts(data));
@@ -18,7 +16,7 @@ function Home() {
   return (
     <div>
       <article>
-        <h3>Hello {user.fbUser.displayName}, welcome to Gear Stop! </h3>
+        <h3>Welcome to Gear Stop! </h3>
         <div className="row row-cols-1 row-cols-md-2">
           {products.map((product) => (
             <section key={`product--${product.id}`} className="col mb-4">
